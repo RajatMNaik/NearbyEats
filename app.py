31053de2818dc281eab4b9c79e9580cc1ab3154e
Rajat Mahesh Naik <44782608+RajatMNaik@users.noreply.github.com>
2023-09-15 13:28:58 +0200

    feat: Added it in Flask App

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-import requests
-import geocoder
+import requests, geocoder
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
 
 def location():
     g = geocoder.ip('me')
     return g.latlng 
 
 def restaurants_nearby(latitude, longitude, radius=100, api_key='API key'):
-    
     endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     params = {
         'location': f"{latitude},{longitude}",
@@ -18,9 +19,17 @@ def restaurants_nearby(latitude, longitude, radius=100, api_key='API key'):
     response = requests.get(endpoint_url, params=params)
     results = response.json().get('results', [])
     
-    for place in results:
-        print(place['name'])
+    return [place['name'] for place in results]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        radius = request.form.get('radius')
+        lat, lon = location()
+        restaurants = restaurants_nearby(lat, lon, radius)
+        return render_template('results.html', restaurants=restaurants)
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    lat, lon = location()
-    restaurants_nearby(lat, lon)
+    app.run(debug=True)
+
